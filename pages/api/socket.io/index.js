26694613daf7f15e74792f53aac5f74208f74348
Michael Dawson <mdawson@devrus.com>
2024-01-11 17:49:10 -0500

    src: add integration with hardware
    Signed-off-by: user1 <user1@localhost.localdomain>

--- pages/api/socket.io/index.js
+++ pages/api/socket.io/index.js
@@ -1,41 +1,57 @@
 import { Server } from 'socket.io';
+import { readFileSync, readdirSync } from 'node:fs';
+import { exec } from 'node:child_process';
+
+const DEVICES_PREFIX = '/sys/bus/w1/devices/';
+const TOP_LEFT_PIN = 17;
+const TOP_RIGHT_PIN = 27;
+const POLL_INTERVAL = 3000;
+
+function getDeviceFile() {
+  // currently assumes there is a single device
+  const entries = readdirSync(DEVICES_PREFIX)
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i].startsWith('28-')) {
+      return DEVICES_PREFIX + entries[i] + '/hwmon/hwmon1/temp1_input';
+    };
+  };
+};
+
+function readTemp(tempSensorFile) {
+  const valueRead = readFileSync(tempSensorFile);
+  return valueRead/1000;
+};
+
+function readGpio(pin, callback) {
+  exec('gpioget gpiochip0 ' + pin, (err, stdout, stderr) => {
+    const pinValueString = stdout.replace(/\s+/g,'');
+
+    let pinValue = false;
+    if (pinValueString === '1')
+      pinValue = true;
+
+    callback(pinValue);
+  });
+};
 
 const SocketHandler = (req, res) => {
+  const tempSensorFile = getDeviceFile();
   if (res.socket.server.io) {
   } else {
-    console.log('Socket is initializing')
-    const io = new Server(res.socket.server, { path: '/api/socket.io'})
-    res.socket.server.io = io
-
-    let temp = 10;
-    let left = false;
-    let right = false;
-    setInterval(() => {
-      temp = temp + 1;
-      if (temp > 20 ) {
-        temp = 10;
-      };
-
-      if (!left && !right) {  
-        left = true;
-        right = false; 
-      } else if (left && !right) {  
-        left = false;
-        right = true;
-      } else if (!left && right) {  
-        left = true;
-        right = true;
-      } else if (left && right) {  
-        left = false;
-        right = false;
-      };
-
-      io.emit('message', {temp: temp, topLeft: left, topRight: right});
-    } , 5000)
-
+    console.log('Socket is initializing');
+    const io = new Server(res.socket.server, { path: '/api/socket.io'});
     res.socket.server.io = io;
-  }
+
+    setInterval(() => {
+      const temp = readTemp(tempSensorFile);
+      readGpio(TOP_LEFT_PIN, (left) => {
+        readGpio(TOP_RIGHT_PIN, (right) => {
+          io.emit('message', {temp: temp, topLeft: left, topRight: right});
+        });
+      });
+    } , POLL_INTERVAL);
+  };
   res.end();
-}
+};
 
 export default SocketHandler;
